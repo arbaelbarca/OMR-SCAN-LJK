0df18e7bd2834e49a6393e634a2dbc453a1afb70
Muhamad Arba <muhamadarba@KTV-2023-132.local>
2024-05-04 15:27:40 +0700

    first commit

---
+++ app/src/main/java/com/apps/arbaelbarca/omrscanner/StartActivity.java
@@ -0,0 +1,40 @@
+package com.apps.arbaelbarca.omrscanner;
+
+import android.app.Dialog;
+import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+
+    Button scan,answer;
+    boolean isCamera = true;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        scan = findViewById(R.id.scan);
+        answer = findViewById(R.id.setAnswers);
+
+        scan.setOnClickListener(v -> {
+            Dialog dialog = new Dialog(StartActivity.this);
+            dialog.setContentView(R.layout.selection_dialog);
+            dialog.findViewById(R.id.camera).setOnClickListener(view->{isCamera=true; dialog.cancel(); openActivity();});
+            dialog.findViewById(R.id.gallery).setOnClickListener(view->{isCamera=false; dialog.cancel();openActivity();});
+            dialog.show();
+//            openActivity();
+        });
+
+
+        answer.setOnClickListener(v -> startActivity(new Intent(StartActivity.this, AnswersActivity.class)));
+    }
+
+    public void openActivity(){
+        Intent intent = new Intent(StartActivity.this,MainActivity.class);
+        intent.putExtra("isCamera",isCamera);
+        startActivity(intent);
+    }
+
+}
